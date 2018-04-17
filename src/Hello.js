09c97a8b28179944dfc1d5fd79eed174147640d6
Temicka Brown <mekapleio@yahoo.com>
2018-04-17 00:24:46 -0400

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+// define our Hello component
+class Hello extends Component {
+  // what should the component render
+  render () {
+    // Make sure to return some UI
+    return (
+      <h1>Hello Temicka</h1>
+    )
+  }
+}
+
+export default Hello
